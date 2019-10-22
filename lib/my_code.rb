e27b8b95fea9ea9cf02ec5cce15ebf538fb5101c
Paul Leugemors <pleugemors@yahoo.com>
2019-10-22 07:48:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,7 +5,7 @@ def map(source_array)
   new_array= []
   row = 0 
   while row <source_array.length do 
-    new_array.push(yield)
+    new_array.push(source_array[row] yield)
     row += 1 
   end
   new_array
